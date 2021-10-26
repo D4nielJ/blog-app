538fa430d238fc904a15ba959bf42d9ff75c3322
d4nielj <d4niel.djm@gmail.com>
2021-10-26 12:56:07 -0500

    Add routing for users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'users/index'
+  root 'users#index'
 
-  resources :users
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
