5e3e2b13381cf36de16569ccd068fc73ccae7c99
d4nielj <d4niel.djm@gmail.com>
2021-10-26 16:29:20 -0500

    Initial styles with tailwindcss

--- app/javascript/tailwind/tailwind.config.js
+++ app/javascript/tailwind/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  enabled: ["production", "staging"].includes(process.env.NODE_ENV),
+  enabled: ['production', 'staging'].includes(process.env.NODE_ENV),
   purge: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
@@ -8,6 +8,15 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: (theme) => ({
+      center: true,
+      padding: {
+        default: theme('spacing.8'),
+        sm: theme('spacing.24'),
+        lg: theme('spacing.40'),
+        xl: theme('spacing.60'),
+      },
+    }),
     extend: {},
   },
   variants: {
