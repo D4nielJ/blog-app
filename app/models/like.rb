5dba6bf43919ae17a8bf1c681ab1be6c473cfb05
d4nielj <d4niel.djm@gmail.com>
2021-11-01 16:43:45 -0500

    Create methods for models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def update_post_likes
+    counter = post.likes.count('id')
+    post.update(likes_counter: counter)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
-  has_many :posts, :comments, :likes
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+
+  def recent_posts(quantity)
+    posts.order(created_at: :desc).limit(quantity)
+  end
+
+  def recent_posts_3
+    recent_posts(3)
+  end
 end
