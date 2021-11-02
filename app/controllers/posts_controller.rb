e662b21f091efe897dd3e4e731596e7ec6556369
d4nielj <d4niel.djm@gmail.com>
2021-11-02 18:02:51 -0500

    Add functionality to show more button in posts index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.limit(3)
+    @limit = params[:limit] unless params[:limit].nil?
   end
 
   def show
