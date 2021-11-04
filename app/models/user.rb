0cea790c73dbbdcc10222bb7000eafa981668fc7
d4nielj <d4niel.djm@gmail.com>
2021-11-04 17:24:31 -0500

    Add spec to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-    def recent_posts(quantity)
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+
+  def recent_posts(quantity)
     posts.order(created_at: :desc).limit(quantity)
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    User.create(name: 'Tom Bombadill',
+                photo: 'link',
+                bio: 'Old Tom Bombadil is a merry fellow! Bright Blue his jacket is, and his boots are yellow!',
+                posts_counter: 0)
+  end
+
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 end
