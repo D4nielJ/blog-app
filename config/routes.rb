db1be4d0153f6d1412b8942d00855bf4da87e918
d4nielj <d4niel.djm@gmail.com>
2021-11-04 02:28:04 -0500

    Remove unused code from routers

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   root 'users#index'
 
-  # resources :users, only: %i[index show] do
-  #   resources :posts, only: %i[index show create]
-  # end
-
-  resources :users do
-    resources :posts do
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show create] do
       resources :comments, only: %i[new create]
     end
   end
