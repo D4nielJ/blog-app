38eeddc3cf1fa4f484a1ced4889e2e4283bf873e
d4nielj <d4niel.djm@gmail.com>
2021-11-04 13:12:15 -0500

    Add bullet gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  def recent_posts(quantity)
+    def recent_posts(quantity)
     posts.order(created_at: :desc).limit(quantity)
   end
 
