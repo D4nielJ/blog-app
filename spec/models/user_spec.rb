d4ec3b42908869b5ad5f59e6bd695791005344a1
d4nielj <d4niel.djm@gmail.com>
2021-11-04 17:51:46 -0500

    fix old tests with dynamic routes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   subject do
-    User.create(name: 'Tom Bombadill',
+    User.create(name: 'Aragorn II Elessar',
                 photo: 'link',
-                bio: 'Old Tom Bombadil is a merry fellow! Bright Blue his jacket is, and his boots are yellow!',
+                bio: "I am Aragorn son of Arathorn, and am called Elessar,\s
+                the Elfstone, Dúnadan, the heir of Isildur Elendil's son of Gondor.\s
+                Here is the sword that was broken and is forged again!",
                 posts_counter: 0)
   end
 
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  before :each do
+    @user = User.create(name: 'Aragorn II Elessar',
+                       photo: 'link',
+                       bio: "I am Aragorn son of Arathorn, and am called Elessar,\s
+                       the Elfstone, Dúnadan, the heir of Isildur Elendil's son of Gondor.\s
+                       Here is the sword that was broken and is forged again!",
+                       posts_counter: 0)
+  end
+
   describe 'GET /index' do
     before { get '/users' }
     it 'is a success' do
@@ -15,7 +24,7 @@ RSpec.describe 'Users', type: :request do
   end
 
   describe 'GET /index/id' do
-    before { get '/users/1' }
+    before { get user_path(@user) }
     it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
