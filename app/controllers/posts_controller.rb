1c0956622b43c26909f80d0a82558a9a8eb5ecfe
d4nielj <d4niel.djm@gmail.com>
2021-11-03 23:55:31 -0500

    Add form to create Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,20 @@ class PostsController < ApplicationController
     @post = @user.posts.find(params[:id])
   end
 
-  def new; end
+  def new
+    @current_user = current_user
+    @post = Post.new
+  end
 
-  def create; end
+  def create
+    @user = current_user
+    @post = @user.posts.create(posts_params)
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def posts_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   root 'users#index'
 
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create]
+  # resources :users, only: %i[index show] do
+  #   resources :posts, only: %i[index show create]
+  # end
+
+  resources :users do
+    resources :posts
   end
+
+  get 'new', to: 'posts#new' # new post
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
