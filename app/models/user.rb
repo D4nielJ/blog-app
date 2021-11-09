ed75531adddf310dc738f032a5170b78a2067d5f
d4nielj <d4niel.djm@gmail.com>
2021-11-08 21:06:25 -0500

    Remove posts_counter validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def recent_posts(quantity)
     posts.order(created_at: :desc).limit(quantity)
