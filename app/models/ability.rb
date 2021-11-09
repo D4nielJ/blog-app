5378c3ecdbf34ada9ee4d71f1fd79481ca78bde2
d4nielj <d4niel.djm@gmail.com>
2021-11-09 14:50:29 -0500

    Add ability.rb document

---
+++ app/models/ability.rb
@@ -0,0 +1,15 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :read, :all  # start by defining rules for all users, also not logged ones
+    return unless user.is? :default
+    can :manage, Post, author_id: user.id # if the user is logged in can manage it's own posts
+    can :manage, Comment, author_id: user.id
+    can :manage, Like, author_id: user.id
+    can :create, Comment # logged in users can also create comments
+    can :create, Post
+    return unless user.is? :admin
+    can :manage, :all # finally we give all remaining permissions only to the admins
+  end
+end
