a8764dc71a0512d656f9c537647993875e079b16
d4nielj <d4niel.djm@gmail.com>
2021-11-10 17:47:15 -0500

    Fix tests from last feature

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   before :each do
-    @user = User.create(name: 'Aragorn II Elessar',
-                        photo: 'link',
-                        bio: "I am Aragorn son of Arathorn, and am called Elessar,\s
-                       the Elfstone, Dúnadan, the heir of Isildur Elendil's son of Gondor.\s
-                       Here is the sword that was broken and is forged again!",
-                        posts_counter: 0)
+    @user = User.new(name: 'Aragorn II Elessar',
+                     email: 'aragorn@user.com',
+                     bio: "I am Aragorn son of Arathorn, and am called Elessar,\s
+                      the Elfstone, Dúnadan, the heir of Isildur Elendil's son of Gondor.\s
+                      Here is the sword that was broken and is forged again!",
+                     password: 'password',
+                     password_confirmation: 'password',
+                     posts_counter: 0)
+    @user.skip_confirmation!
+    @user.save!
   end
 
   describe 'GET /index' do
