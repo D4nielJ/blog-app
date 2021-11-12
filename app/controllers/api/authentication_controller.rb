209fac0a3c3d0f589dedce52d66556f6f9bf3db3
d4nielj <d4niel.djm@gmail.com>
2021-11-12 11:50:14 -0500

    Add method to create comments from API

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -1,6 +1,6 @@
 class Api::AuthenticationController < ApplicationController
   skip_before_action :authorize_request, only: :authenticate
-  skip_before_action :verify_authenticity_token, only: :authenticate
+  # skip_before_action :verify_authenticity_token, only: :authenticate
 
   # return auth token once user is authenticated
   def authenticate
--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -6,32 +6,24 @@ class Api::CommentsController < ApplicationController
     render json: @comments
   end
 
-  # def new; end
+  def create
+    user = current_user
+    comment = Comment.new(comment_params)
+    comment.author = user
+    response = if comment.save
+                 { comment: comment }
+               else
+                 { message: "comment didn't save" }
+               end
+    json_response(response)
+  end
 
-  # def create
-  #   @current_user = current_user
-  #   @user = User.find(params[:user_id])
-  #   @post = Post.find(params[:post_id])
-  #   @comment = Comment.new(comments_params)
-  #   @comment.post = @post
-  #   @comment.author = @current_user
+  private
 
-  #   flash[:error] = @comment.errors.messages unless @comment.save
-  #   redirect_to user_post_path(@user, @post)
-  # end
-
-  # def destroy
-  #   @user = User.find(params[:user_id])
-  #   @post = @user.posts.find(params[:post_id])
-  #   @comment = @post.comments.find(params[:id])
-  #   @comment.destroy
-  #   flash[:notice] = 'Comment deleted succesfully'
-  #   redirect_to user_post_path(@user, @post)
-  # end
-
-  # private
-
-  # def comments_params
-  #   params.require(:comment).permit(:text)
-  # end
+  def comment_params
+    params.permit(
+      :text,
+      :post_id
+    )
+  end
 end
